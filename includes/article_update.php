a325a52dfeca9a0c40c81c54068b24162f4cf6aa
Nikita Lemshko <superbit670@gmail.com>
2019-06-25 20:23:54 +0700

    del

--- includes/article_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-$id=$_POST['id'];
-$head=$_POST['head'];
-$short=$_POST['short'];
-$text=$_POST['text'];
-
-$result = mysqli_query($connection," UPDATE `articles` SET `head` = '$head', `short`='$short', `text` = '$text' WHERE `articles`.`id` = '$id';");
-header("Location:../article_page.php?id=".$id);
--- includes/comment_add.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include 'connection.php';
-$login=$_SESSION['login'];
-$id=$_POST['id'];
-$text=$_POST['comment'];
-mysqli_query($connection," INSERT INTO `comments` (`id`, `author`, `text`, `article_id`, `pubdate`) VALUES (NULL, '$login', '$text', '$id', CURRENT_TIMESTAMP)");
-header("Location:../article_page.php?id=$id");
